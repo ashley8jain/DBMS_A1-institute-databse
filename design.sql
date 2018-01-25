596a800508e39d7a985b93c8431d767c6acedf4a
Ashley Jain <ashley8jain@gmail.com>
2018-01-25 22:04:49 +0530

    change constraint

--- design.sql
+++ design.sql
@@ -1,27 +1,27 @@
 CREATE TABLE student (
-  student_id serial PRIMARY KEY,
+  student_id varchar(50) PRIMARY KEY,
   name varchar(50) NOT NULL
 );
 CREATE TABLE teacher (
-  teacher_id serial PRIMARY KEY,
+  teacher_id varchar(50) PRIMARY KEY,
   name varchar(50) NOT NULL
 );
 CREATE TABLE course (
-  course_id serial PRIMARY KEY,
+  course_id varchar(50) PRIMARY KEY,
   name varchar(150) NOT NULL
 );
 CREATE TABLE section (
   section_number char(1),
-  course_id int REFERENCES course,
+  course_id varchar(50) REFERENCES course,
   CONSTRAINT valid_section CHECK (section_number in ('A','B','C','D'))
 );
 CREATE TABLE registers (
-  student_id int REFERENCES student ON DELETE CASCADE,
-  course_id int REFERENCES course ON DELETE CASCADE,
+  student_id varchar(50) REFERENCES student ON DELETE CASCADE,
+  course_id varchar(50) REFERENCES course ON DELETE CASCADE,
   CONSTRAINT registers_pk PRIMARY KEY(student_id,course_id)
 );
 CREATE TABLE teaches (
-  teacher_id int REFERENCES teacher ON DELETE CASCADE,
-  course_id int REFERENCES course ON DELETE CASCADE,
+  teacher_id varchar(50) REFERENCES teacher ON DELETE CASCADE,
+  course_id varchar(50) REFERENCES course ON DELETE CASCADE,
   CONSTRAINT teaches_pk PRIMARY KEY(teacher_id,course_id)
 );
--- test_queries.sql
+++ test_queries.sql
@@ -1,8 +1,34 @@
-INSERT INTO student(name) VALUES ('Akshay Kumar');
-INSERT INTO teacher(name) VALUES ('Anshul Kumar');
-INSERT INTO course(name) VALUES ('Inro. to Electrical');
-INSERT INTO section(section_number,course_id) VALUES ('A',1);
-INSERT INTO registers(student_id,course_id) VALUES (1,1);
+INSERT INTO student(student_id,name) VALUES ('2014CS50280','Akshay Kumar');
+INSERT INTO student(student_id,name) VALUES ('2015CS50281','Vinay Kumar');
+
+INSERT INTO teacher(teacher_id,name) VALUES ('anshulk','Anshul Kumar');
+INSERT INTO teacher(teacher_id,name) VALUES ('subhodhs','Subhodh Sharma');
+INSERT INTO teacher(teacher_id,name) VALUES ('sak','S.Arun Kumar');
+
+INSERT INTO course(course_id,name) VALUES ('EEL100','Inro. to Electrical');
+INSERT INTO course(course_id,name) VALUES ('COL321','TOC');
+INSERT INTO course(course_id,name) VALUES ('COL341','Discrete math');
+
+INSERT INTO section(section_number,course_id) VALUES ('A','EEL100');
+INSERT INTO section(section_number,course_id) VALUES ('B','EEL100');
+INSERT INTO section(section_number,course_id) VALUES ('A','COL321');
+INSERT INTO section(section_number,course_id) VALUES ('B','COL321');
+INSERT INTO section(section_number,course_id) VALUES ('C','COL321');
+
+INSERT INTO registers(student_id,course_id) VALUES ('2014CS50280','EEL100');
+INSERT INTO registers(student_id,course_id) VALUES ('2014CS50280','COL321');
+INSERT INTO registers(student_id,course_id) VALUES ('2014CS50280','COL341');
+INSERT INTO registers(student_id,course_id) VALUES ('2015CS50281','EEL100');
+INSERT INTO registers(student_id,course_id) VALUES ('2015CS50281','COL341');
 -- INSERT INTO registers(student_id,course_id) VALUES (1,1);
-INSERT INTO teaches(teacher_id,course_id) VALUES (1,1);
-DELETE FROM student WHERE student_id = 1;
+
+INSERT INTO teaches(teacher_id,course_id) VALUES ('anshulk','EEL100');
+INSERT INTO teaches(teacher_id,course_id) VALUES ('subhodhs','COL321');
+INSERT INTO teaches(teacher_id,course_id) VALUES ('sak','EEL100');
+INSERT INTO teaches(teacher_id,course_id) VALUES ('sak','COL341');
+
+SELECT registers.student_id,registers.course_id,student.name FROM student,registers
+WHERE registers.student_id = student.student_id ;
+
+-- DELETE FROM student WHERE student_id = 1;
+-- DELETE FROM registers WHERE student_id = 2 AND course_id = 1;
