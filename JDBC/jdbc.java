39eead376c66468909790cd8a8e01f8c5e96a54b
Ashley Jain <ashley8jain@gmail.com>
2018-01-29 17:31:06 +0530

    JDBC programming

---
+++ JDBC/jdbc.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+public class jdbc {
+  public static void main(String[] args) throws Exception {
+    System.out.println("hello");
+    String DB_URL = "jdbc:postgresql://localhost:5432/assign1";
+
+    String USER = "postgres";
+    String PASS = args[0];
+    Connection conn = null;
+    try{
+      //Register JDBC driver
+      Class.forName("org.postgresql.Driver");
+
+      //Open a connection
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+      //Create query statement
+      System.out.println("Creating statement...");
+      String query = "INSERT INTO registers(student_id,course_id) VALUES (?,?)";
+      PreparedStatement ps = conn.prepareStatement(query);
+
+      //Read csv file
+      String fileName= "../tuples.csv";
+      File file= new File(fileName);
+      List<List<String>> tuples = new ArrayList<>();
+      Scanner inputStream;
+
+      inputStream = new Scanner(file);
+      inputStream.next();
+      while(inputStream.hasNext()){
+          String tuple = inputStream.next();
+          String[] values = tuple.split(",");
+          // this adds the currently parsed line to the 2-dimensional string array
+          tuples.add(Arrays.asList(values));
+      }
+      inputStream.close();
+      // System.out.println("here: "+tuples.get(0).get(0));
+
+      //Execute Batch Insert
+      for(List<String> tuple: tuples){
+        ps.setString(1, tuple.get(0));
+      	ps.setString(2, tuple.get(1));
+      	ps.addBatch();
+      }
+
+      //Execute a query
+      ps.executeBatch();
+      ps.close();
+
+      conn.close();
+
+    }
+    catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+
+    System.out.println("Goodbye!");
+  }
+}
---
+++ JDBC/script.sh
@@ -0,0 +1,2 @@
+javac -cp ".:./postgresql-42.2.1.jar" jdbc.java
+java -cp ".:./postgresql-42.2.1.jar" jdbc $1
